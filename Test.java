c3dac1f1ab80afae210d6b3dd510c5ff515965d1
JasLeu <jml858@hotmail.com>
2022-08-19 14:08:19 -0700

    test

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test
+{
+    public static void main(String[] args)
+    {
+        String cool = "Welcome";
+        System.out.println("Jason");
+        System.out.println("Junior");
+        System.out.printf(cool);
+    }
+}
