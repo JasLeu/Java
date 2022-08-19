e447f8e8d82964388438203128800ff478d76f1e
JasLeu <jml858@hotmail.com>
2022-08-19 14:32:11 -0700

    hi

--- Test.java
+++ Test.java
@@ -2,9 +2,7 @@ public class Test
 {
     public static void main(String[] args)
     {
-        String cool = "Welcome";
         System.out.println("Jason");
         System.out.println("Junior");
-        System.out.printf(cool);
     }
 }
