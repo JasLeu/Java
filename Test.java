4e65c71b8c38ef3bd2a00973ed34da213ab11e7e
JasLeu <jml858@hotmail.com>
2022-08-23 14:15:30 -0700

    test

--- Test.java
+++ Test.java
@@ -2,8 +2,6 @@ public class Test
 {
     public static void main(String[] args)
     {
-        System.out.println("Jason");
-        System.out.println("Junior");
         String greeting = "Welcome";
         System.out.println("Jason");
         System.out.println("Junior");
