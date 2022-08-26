7e1c6919a5d079618f9ce37dcbe2cb990eea9a6e
JasLeu <jml858@hotmail.com>
2022-08-26 14:11:32 -0700

    made a number operation program

---
+++ NumberRiddle.java
@@ -0,0 +1,16 @@
+public class NumberRiddle {
+    public static void main(String[] args) {
+        int init = 1;
+        int num = init;
+        System.out.println("num = " + num);
+        num *= 2;
+        System.out.println("num = " + num);
+        num += 6;
+        System.out.println("num = " + num);
+        double numb = num;
+        numb /= 2;
+        System.out.println("num = " + numb);
+        numb -= init;
+        System.out.println("num = " + numb);
+    }
+}
