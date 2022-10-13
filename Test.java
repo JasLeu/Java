060b546268a9aae9ab3de82304db2b6e64b708b7
JasLeu <111610717+JasLeu@users.noreply.github.com>
2022-10-12 20:54:21 -0700

    program: change letter to another letter (d,b,p,q)

--- Test.java
+++ Test.java
@@ -10,19 +10,25 @@ public class Test
   public static void main(String[] args)
   {
     Scanner input = new Scanner(System.in);
+    ArrayList<String> vowels = new ArrayList<String>();
+
+    Collections.addAll(vowels, "d", "b", "p", "q");
     System.out.print("String: ");
     String stringInput = input.nextLine();
     int i = 0;
 
     while (i < stringInput.length()-1) {
-        String doubleSpace = stringInput.substring(i, i+2);
-        if (doubleSpace.equals("  ")) {
-            stringInput = stringInput.substring(0, i) + stringInput.substring(i+1);
+        int x = stringInput.indexOf(stringInput.substring(i, i+1));
+        if (vowels.contains(stringInput.substring(x,x+1))) {
+            String newVowel = vowels.get((int)(Math.random()*4));
+            System.out.println(newVowel);
+            stringInput = stringInput.substring(0, i) + newVowel + stringInput.substring(i+1);
         }
         i++;
     }
 
     System.out.println(stringInput);
+    input.close();
   }
   
 }
