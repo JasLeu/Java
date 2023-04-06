9d58932880afd6182cce13a693348ba0be9b3ede
JasLeu <jml858@hotmail.com>
2023-04-06 14:01:37 -0700

    4/5/2023 Project 1

--- SteganographyLab/StegoLab_Code/PictureTester.java
+++ SteganographyLab/StegoLab_Code/PictureTester.java
@@ -30,12 +30,96 @@ public class PictureTester extends PixelPlayground {
         return newPic;
     }
 
+    public static Picture mirrorVer1(Picture p) {
+        Picture newPic = new Picture(p);
+        Pixel[][] pixels = newPic.getPixels2D();
+
+        for (int i = 0; i < pixels.length-1; i++) {
+            for (int j = 0; j < pixels[0].length/2-1; j++) {
+                pixels[i][pixels[0].length - j - 1].setColor(pixels[i][j].getColor());
+
+            }
+        }
+        return newPic;
+    }
+
+    public static Picture mirrorVer2(Picture p) {
+        Picture newPic = new Picture(p);
+        Pixel[][] pixels = newPic.getPixels2D();
+
+        for (int i = 0; i < pixels.length-1; i++) {
+            for (int j = 0; j < pixels[0].length/2-1; j++) {
+                pixels[i][j].setColor(pixels[i][pixels[0].length - j - 1].getColor());
+
+            }
+        }
+        return newPic;
+    }
+
+    public static Picture mirrorHor1(Picture p) {
+        Picture newPic = new Picture(p);
+        Pixel[][] pixels = newPic.getPixels2D();
+
+        for (int i = 0; i < pixels.length / 2 - 1; i++) {
+            for (int j = 0; j < pixels[0].length - 1; j++) {
+                pixels[pixels.length - i - 1][j].setColor(pixels[i][j].getColor());
+
+            }
+        }
+        return newPic;
+    }
+
+    public static Picture mirrorHor2(Picture p) {
+        Picture newPic = new Picture(p);
+        Pixel[][] pixels = newPic.getPixels2D();
+
+        for (int i = 0; i < pixels.length / 2 - 1; i++) {
+            for (int j = 0; j < pixels[0].length - 1; j++) {
+                pixels[i][j].setColor(pixels[pixels.length - i - 1][j].getColor());
+
+            }
+        }
+        return newPic;
+    }
+
+    public static Picture mirrorDiagonal(Picture p) {
+        Picture newPic = new Picture(p);
+        Pixel[][] pixels = newPic.getPixels2D();
+
+        for (int row = 0; row < pixels.length; row++) {
+            for (int col = 0; col < pixels.length; col++) {
+
+                pixels[row][col].setColor(pixels[col][row].getColor());
+
+            }
+        }
+        return newPic;
+    }
+
+    public static Picture mirrorDiagonal2(Picture p) {
+        Picture newPic = new Picture(p);
+        Pixel[][] pixels = newPic.getPixels2D();
+
+        for (int row = 0; row < pixels.length; row++) {
+            for (int col = 0; col < pixels.length; col++) {
+
+                pixels[col][row].setColor(pixels[pixels.length - row - 1][pixels.length-col].getColor());
+
+            }
+        }
+        return newPic;
+    }
+
     public static void main(String[] args) {
         Picture beach = new Picture("beach.jpg");
         //Picture negated = negate(beach);
         //negated.explore();
-        Picture gray = grayScale(beach);
-        gray.explore();
+        //Picture gray = grayScale(beach);
+        //gray.explore();
+        //Picture mirrored = mirrorVer1(beach);
+        //mirrored.explore();
+        Picture mirrored = mirrorDiagonal2(beach);
+        mirrored.explore();
     }
     
 }
